1b69fa7235bb7bacd7973dca46a1ec355f704b65
Jonathan Garuti <jonathan_garuti@hotmail.com>
2024-02-19 16:16:22 -0300

    Menu adicionado

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,5 +1,24 @@
 package conta;
 
+import conta.model.conta;
+
 public class Menu {
 
+	public static void main(String[] args) {
+		
+		conta c1 = new conta(1, 123, 1, "User", 800000.0f);
+		
+		
+		c1.visualizar();
+		
+		
+		if (c1.sacar(100))
+		System.out.println("Saque efetuado com sucesso. O novo saldo é de: " + c1.getSaldo());
+		else
+			System.out.println("O Saldo é insuficiente!");
+		
+		c1.depositar(1000);
+		System.out.println("O novo saldo é: " + c1.getSaldo());
+		
+	}
 }
